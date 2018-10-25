11e6eb481dce09e8b8bb25f7ab82616e1ae5fce3
Andri K. Marengga <andree.marengga@gmail.com>
2018-10-25 20:21:44 +0700

    Create blank reducers

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Header } from './src/components/common';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Header } from "./src/components/common";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducers from "./src/reducers";
 
 export default class App extends React.Component {
   render() {
     return (
-      <View>
-        <Header headerText="Tech Stack" />
-      </View>
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Header headerText="Tech Stack" />
+        </View>
+      </Provider>
     );
   }
 }
